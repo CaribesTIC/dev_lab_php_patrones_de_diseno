ffcbde29568f8319f578f615adcdd9b820bac8ff
Ernesto Canquiz <fdsoil123@gmail.com>
2020-04-27 13:58:31 -0400

    Creational Patterns

---
+++ CreationalPatterns/01FactoryAbstrac1_ok/BookFactory.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * BookFactory classes
+ */
+abstract class AbstractBookFactory {
+    abstract function makePHPBook();
+    abstract function makeMySQLBook();
+}
+
+class OReillyBookFactory extends AbstractBookFactory {
+    private $context = "OReilly";
+    function makePHPBook() {
+        return new OReillyPHPBook;
+    }
+    function makeMySQLBook() {
+        return new OReillyMySQLBook;
+    }
+}
+
+class SamsBookFactory extends AbstractBookFactory {
+    private $context = "Sams";
+    function makePHPBook() {
+        return new SamsPHPBook;
+    }
+    function makeMySQLBook() {
+        return new SamsMySQLBook;
+    }
+}
+
+
+
+
+
